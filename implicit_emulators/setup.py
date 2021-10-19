45352f214ec28c9f91dd24ed3669f492d8b68382
mnonnenm <marcel.nonnenm@gmail.com>
2021-10-19 09:27:02 +0200

    Update setup.py
    de-anonymizing code package.

--- implicit_emulators/setup.py
+++ implicit_emulators/setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='implem_env',
       version='0.1',
       description='Implicitly integraded emulators in pytorch',
-      url='',
-      author='',
-      author_email='',
+      url='https://github.com/m-dml/lil2021swe',
+      author='Marcel Nonnenmacher',
+      author_email='marcel.nonnenm@gmail.com',
       license='apache',
       packages=['implem'],
       zip_safe=False)
+ 
